ded60c50c0ee0a3cf7c95dcbf23834ffe2d0b81c
pjotr <pjotrscholtze1@gmail.com>
2021-02-06 12:04:49 +0100

    removed redundent extracting

--- experiments/project_generators/valid_query_finder.py
+++ experiments/project_generators/valid_query_finder.py
@@ -9,12 +9,8 @@ base_raw = """
         "#!/bin/bash -e",
         "#SBATCH -t 120:00 -N 1 -n 8 --mem=16000M",
         "#SBATCH --output=$PROJECT_PATH/slurm_%j.out",
-        "rm -rf $PROJECT_PATH/db",
-        "time $PROJECT_PATH/trident/trident load -i $PROJECT_PATH/db -f /var/scratch/pse740/latest-lexemes.nt.gz",
-        "du -h -d0 $PROJECT_PATH/db",
-        "tar xvf $PROJECT_PATH/trident/experiments/queries/chunked_queries.tar.gz -C $PROJECT_PATH/trident/experiments/queries",
-        "$PROJECT_PATH/trident/trident benchmark -i $PROJECT_PATH/db --query_type query_native --query_file $PROJECT_PATH/trident/experiments/queries/queries/query_chunk_0.sparql --results_file $PROJECT_PATH/res.json.lines --repetitions 30",
-        "rm -rf $PROJECT_PATH/db",
+        "du -h -d0 $DATABASE_PATH/latest-lexemes",
+        "$BUILD_CACHE_PATH/trident/trident benchmark -i $DATABASE_PATH/latest-lexemes --query_type query_native --query_file $PROJECT_PATH/trident/experiments/queries/queries/query_chunk_0.sparql --results_file $PROJECT_PATH/res.json.lines --repetitions 30",
         "rm -rf $PROJECT_PATH/trident"
     ]
 }
@@ -24,7 +20,7 @@ res = []
 for i in range(0,92):
     data = json.loads(base_raw)
     data["name"] = "validate-query-%d" % i
-    data["script"][7] = "$PROJECT_PATH/trident/trident benchmark -i $PROJECT_PATH/db --query_type query_native --query_file $PROJECT_PATH/trident/experiments/queries/queries/query_chunk_%d.sparql --results_file $PROJECT_PATH/res.json.lines --repetitions 30" % i
+    data["script"][4] = "$BUILD_CACHE_PATH/trident/trident benchmark -i $PROJECT_PATH/db --query_type query_native --query_file $PROJECT_PATH/trident/experiments/queries/queries/query_chunk_%d.sparql --results_file $PROJECT_PATH/res.json.lines --repetitions 30" % i
     res.append(data)
 
 with open("projects/query-validation.json", "w") as f:
