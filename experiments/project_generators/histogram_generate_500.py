921e5623a88d28e2354d059e1ca09b4c1310b90d
pjotr <pjotrscholtze1@gmail.com>
2021-02-24 17:05:38 +0100

    histogram job

---
+++ experiments/project_generators/histogram_generate_500.py
@@ -0,0 +1,27 @@
+import json
+base_raw = """
+{
+    "name": "histogram-generate-500",
+    "description": "Generate histogram/counters for index and table usage, repetition will be 5 times",
+    "github_url": "https://github.com/pjotrscholtze/trident.git",
+    "github_checkout": "master",
+    "script": [
+        "#!/bin/bash -e",
+        "#SBATCH -t 15:00 -N 1 -n 8 --mem=16000M",
+        "#SLURM -p longq",
+        "#SBATCH --output=$PROJECT_PATH/slurm_%j.out",
+        "du -h -d0 $DATABASE_PATH/latest-lexemes-skipTables",
+        "__REPLACED_BELOW__"
+    ]
+}
+"""
+
+res = []
+for i in range(0, 913):
+    data = json.loads(base_raw)
+    data["name"] = "base-query-%d-skiptables-500" % i
+    data["script"][5] = "$BUILD_CACHE_PATH/trident/trident benchmark -i $DATABASE_PATH/latest-lexemes-skipTables --query_type query_native --query_file $BUILD_CACHE_PATH/trident/experiments/queries/queries-500/query_chunk_%d.sparql --results_file $PROJECT_PATH/res.json.lines --repetitions 5" % i
+    res.append(data)
+
+with open("projects/histogram-generate-500.json", "w") as f:
+    f.writelines([json.dumps(res, indent=2)])
