79002fe7a6cae3c7ce568b53e894671b6bbdfc64
pjotr <pjotrscholtze1@gmail.com>
2021-03-23 12:07:16 +0100

    tackling performance issues

--- experiments/project_generators/histogram_per_query_generate_500_dbpedia_cleaned_full_nindices_1.py
+++ experiments/project_generators/histogram_per_query_generate_500_dbpedia_cleaned_full_nindices_1.py
@@ -7,7 +7,7 @@ base_raw = """
     "github_checkout": "master",
     "script": [
         "#!/bin/bash -e",
-        "#SBATCH -t 15:00 -N 1 -n 8 --mem=16000M",
+        "#SBATCH -t 15:00 -N 1 -n 8 --mem=64000M",
         "#SBATCH -p longq",
         "#SBATCH --output=$PROJECT_PATH/slurm_%j.out",
         "du -h -d0 $DATABASE_PATH/dbpedia-times-details-nindices-1",
@@ -17,10 +17,10 @@ base_raw = """
 """
 
 res = []
-for i in range(0, 913):
+for i in range(0, 9128):
     data = json.loads(base_raw)
     data["name"] = data["name"] % i
-    data["script"][5] = "$BUILD_CACHE_PATH/trident/trident benchmark -i $DATABASE_PATH/dbpedia-times-details-nindices-1 --query_type query_native --query_file $BUILD_CACHE_PATH/trident/experiments/queries_full/query_chunk_%d.sparql --results_file $PROJECT_PATH/res.json.lines --histogram_mode generate --histogram_file $PROJECT_PATH/temp.json --repetitions 1" % i
+    data["script"][5] = "$BUILD_CACHE_PATH/trident/trident benchmark -i $DATABASE_PATH/dbpedia-times-details-nindices-1 --query_type query_native --query_file $BUILD_CACHE_PATH/trident/experiments/queries_full_small_chunks/query_chunk_%d.sparql --results_file $PROJECT_PATH/res.json.lines --histogram_mode generate --histogram_file $PROJECT_PATH/temp.json --repetitions 1" % i
     res.append(data)
 
 with open("projects/histogram-generate-per-query-500-dbpedia-cleaned-full-nindices-1.json", "w") as f:
