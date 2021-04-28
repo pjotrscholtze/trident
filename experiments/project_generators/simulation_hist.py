a16a5ea75b8b36db0a436aa76cd31424b7dfad3e
pjotr <pjotrscholtze1@gmail.com>
2021-04-28 11:54:31 +0200

    venv

--- experiments/project_generators/simulation_hist.py
+++ experiments/project_generators/simulation_hist.py
@@ -13,6 +13,7 @@ base_raw = """
         "du -h -d0 $DATABASE_PATH/dbpedia-times-details-nindices-1",
         "__REPLACED_BELOW__",
         "__REPLACED_BELOW__",
+        "__REPLACED_BELOW__",
         "__REPLACED_BELOW__"
     ]
 }
@@ -42,8 +43,9 @@ for seed in [10, 4156, 2536, 2123, 7058, 1087, 8306]:
         data["name"] = data["name"] % i
         # python/3.6.0
         data["script"][5] = "module load python/3.6.0"
-        data["script"][6] = "pip3 install -r $BUILD_CACHE_PATH/trident/experiments/simulation/histogram/requirements.txt"
-        data["script"][7] = "python3 $BUILD_CACHE_PATH/trident/experiments/simulation/histogram/main.py %s" % cmd
+        data["script"][6] = "python3 -m venv venv; source ./venv/bin/activate"
+        data["script"][7] = "pip3 install -r $BUILD_CACHE_PATH/trident/experiments/simulation/histogram/requirements.txt"
+        data["script"][8] = "python3 $BUILD_CACHE_PATH/trident/experiments/simulation/histogram/main.py %s" % cmd
         res.append(data)
         i+=1
 
