d18fe7a04e2553d3c77e51d8769e725346f33f52
pjotr <pjotrscholtze1@gmail.com>
2021-05-01 16:56:45 +0200

    index out of bounds

--- experiments/simulation/histogram/constructionAlgorithm/EquiWidthConstructionAlgorithm.py
+++ experiments/simulation/histogram/constructionAlgorithm/EquiWidthConstructionAlgorithm.py
@@ -13,21 +13,21 @@ from typing import List
 #  */
 class EquiWidthConstructionAlgorithm(ConstructionAlgorithm):
 
-    # def create_histogram(self, data: List[tuple[any, float]], bucketNum:int,
+    # def create_histogram(self, data: List[tuple[any, float]], bucket_num:int,
     #     partitionRule: PartitionRule) -> List[Bucket]:
-    def create_histogram(self, data: List, bucketNum:int,
+    def create_histogram(self, data: List, bucket_num:int,
         partitionRule: PartitionRule) -> List[Bucket]:
-        buckets = [Bucket([]) for i in range(0, bucketNum)]
+        buckets = [Bucket([]) for i in range(0, bucket_num)]
         if not data:
             return buckets
 
-        min: float = data[0][1]
-        max: float = data[len(data)-1][1]
-        step: float = (max - min) / float(bucketNum)
+        min_value: float = data[0][1]
+        max_value: float = data[len(data)-1][1]
+        step: float = (max_value - min_value) / float(bucket_num)
 
         for d in data:
-            b: int = int((d[1] - min) / step)
-            if b >= bucketNum: b = bucketNum - 1
+            b: int = int(max(0, (d[1] - min_value) / step))
+            if b >= bucket_num: b = bucket_num - 1
             buckets[b].data.append(d)
 
         return buckets
