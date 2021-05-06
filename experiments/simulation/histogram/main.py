1c2f7361225d60cb6d529ae8edf99048a6716010
pjotr <pjotrscholtze1@gmail.com>
2021-05-06 20:12:45 +0200

    changed paths for das

--- experiments/simulation/histogram/main.py
+++ experiments/simulation/histogram/main.py
@@ -73,8 +73,8 @@ class Histogram:
 logging.basicConfig(level=logging.INFO)
 
 def get_buckets() -> Dict[str, int]:
-    # CACHE_PATH = "/home/pse740/trident/trident_get_buckets.tmp"
-    CACHE_PATH = "/tmp/trident_get_buckets.tmp"
+    CACHE_PATH = "/home/pse740/trident/trident_get_buckets.tmp"
+    # CACHE_PATH = "/tmp/trident_get_buckets.tmp"
     if os.path.exists(CACHE_PATH):
         print("bucket cache not found: ", CACHE_PATH)
         with open(CACHE_PATH, "r")as f:
@@ -90,7 +90,8 @@ def get_buckets() -> Dict[str, int]:
         archive.close()
 
     res = {}
-    files = glob("/storage/wdps/trident/experiments/results/acquiremeasurements/acquire_measurements_sample.7z/acquire_measurements_sample-*/res.json.lines.7z")
+    # files = glob("/storage/wdps/trident/experiments/results/acquiremeasurements/acquire_measurements_sample.7z/acquire_measurements_sample-*/res.json.lines.7z")
+    files = glob("/var/scratch/pse740/acquire_measurements_full-*/res.json.lines.7z")
     for i, resline_path in enumerate(sorted(files)):
         logging.info("Processing: (%.1f procent) %s" % ((i/len(files)) * 100, resline_path))
         res[resline_path] =  sum([s.count(b'\n') for s in _(resline_path)])
@@ -115,8 +116,8 @@ class QueryPicker:
             yield self.get_single()
     
 def get_data(filepath: str, index: List) -> List[Dict[str, any]]:
-    filepath = "/storage/wdps/trident/experiments" + filepath[1:]
-    # filepath = "/var/scratch/pse740/" + (filepath[61:].replace("acquire_measurements_sample", "acquire_measurements_full"))
+    # filepath = "/storage/wdps/trident/experiments" + filepath[1:]
+    filepath = "/var/scratch/pse740/" + (filepath[61:].replace("acquire_measurements_sample", "acquire_measurements_full"))
 
     archive = py7zr.SevenZipFile(filepath, mode='r')
 
